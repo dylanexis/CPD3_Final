cfde942b4653e2165589e3fa40c07e399f0b77ce
Dylan James <dyla@umich.edu>
2024-10-23 21:36:12 -0400

    Please

--- csv_to_meets_html.py
+++ csv_to_meets_html.py
@@ -30,7 +30,7 @@ def csv_to_html(csv_filename, output_folder):
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{link_text}</title>
 <link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="../css/style.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap">
