3352b0e858f93fc0e494a7acf801d34b6587b46c
Dylan James <dyla@umich.edu>
2024-11-09 21:19:28 -0500

    more changes git push

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("Carousel");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 3 seconds
-}
-
-
 document.querySelectorAll('img').forEach(img => {
     console.log("runs");
     img.onerror = function() {
